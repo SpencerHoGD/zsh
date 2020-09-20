da9d171c76f44972219fcddf825ed7596317a6ba
sp@rpi4i3 <125423559@qq.com>
2020-09-21 05:44:18 +0800

    add all file from rpi4i3

---
+++ aliases.zsh
@@ -0,0 +1,52 @@
+alias S='source ~/.zshrc'
+alias ra='ranger'
+alias lg='lazygit'
+alias s='neofetch'
+alias nv='nvim'
+alias neo='neofetch'
+alias sudo='sudo '
+alias X='chmod +x'
+
+
+alias c='clear'
+alias gcl='git clone'
+alias a='~'
+alias gg='~/github/'
+alias gd='~/Downloads/'
+alias gc='~/.config/'
+
+
+# alias av='source venv/bin/activate'
+# alias cdiff='colordiff'
+# alias cs='calcurse'
+# alias dv='deactivate'
+# alias gc='git config credential.helper store'
+# alias ipy='ipython'
+# alias l='ls -la'
+# alias lg='lazygit'
+# alias ms='mailsync'
+# alias mt='neomutt'
+# alias r='echo $RANGER_LEVEL'
+# alias pu='python3 -m pudb'
+# alias ra='ranger'
+# # ra() {
+# 	#if [ -z "$RANGER_LEVEL" ]
+# 	#then
+# 		#ranger
+# 	#else
+# 		#exit
+# 	#fi
+# #}
+# alias s='neofetch'
+# alias g='onefetch'
+# alias sra='sudo -E ranger'
+# alias sudo='sudo -E'
+# alias gs='git config credential.helper store'
+# alias bat='sudo tlp bat'
+# alias ac='sudo tlp ac'
+# alias gy='git-yolo'
+# alias nb='newsboat -r'
+# alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
+# alias ta='tmux a'
+# alias t='tmux'
+# alias lo='lsof -p $(fps) +w'
---
+++ env.zsh
@@ -0,0 +1,42 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export LOCALPROG=$HOME/prog
+export GOPATH=$HOME/go
+
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.gem/ruby/2.7.0
+export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:$LOCALBIN
+export MANPATH="/usr/local/man:$MANPATH"
+export TERM='xterm-256color'
+export RANGER_LOAD_DEFAULT_RC="false"
+export EDITOR=nvim
+export TERM_ITALICS=true
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+# export FZF_DEFAULT_OPTS="\
+    # --height 40% --layout reverse --info inline --border \
+    # --preview 'file {}' --preview-window down:1:noborder \
+    # --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'"
+
+# export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+
+
+
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/linuxbrew-bottles
+
+# export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+# export PATH=$PATH:/home/linuxbrew/.linuxbrew/sbin
+# export PATH=$PATH:$HOME/.linuxbrew/bin
+# export PATH=$PATH:$HOME/.linuxbrew/sbin
+# export PATH=$PATH:$HOME/.local/bin
+# export PATH=$PATH:/usr/local/Cellar/node/14.2.0/bin
+# export PATH=$PATH:$HOME/.cargo/bin
+# export PATH=$PATH:/snap/bin
+# export PATH=$PATH:/usr/local/opt/node@12/bin
+# export PATH=$PATH:$LOCALPROG/flutter/bin
+# export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+# export PATH="$PATH":"$HOME/.pub-cache/bin"
+# export ZSH_AUTOSUGGEST_USE_ASYNC=1
+# export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
